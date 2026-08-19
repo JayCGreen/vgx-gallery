a8bc0312682600ba2a456d8fc1b54201b5d19bd9
Joseph Green <josephg896@gmail.com>
2026-08-19 12:19:56 -0500

    Testing DB stuff part 2

--- src/app/gallery/galleryGrid.jsx
+++ src/app/gallery/galleryGrid.jsx
@@ -2,18 +2,19 @@
  * 
  * @returns A grid of the items listed in the gallery database
  */
+import { getCloudflareContext } from "@opennextjs/cloudflare";
 
 export default async function GalleryGrid() {
     //TO-DO Handle filtering
-      async function fetch(request, env) {
-        const result = await process.env.
+      async function grab() {
+        const result = await getCloudflareContext().env.
             vgx_feed.prepare(
                 "SELECT * FROM Tags",
-            ).run();
+            ).run()
         return new Response(JSON.stringify(result));
     }
     console.log("Testing, testing")
-    console.log("Hye look", fetch({}))
+    console.log("Hye look", grab())
     return (<>
 
     </>)
