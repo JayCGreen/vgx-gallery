5402596002719d376e2770a26c1e3afb73b0c185
Joseph Green <josephg896@gmail.com>
2026-08-19 07:32:52 -0500

    Testing DB stuff

--- src/app/gallery/galleryGrid.jsx
+++ src/app/gallery/galleryGrid.jsx
@@ -3,8 +3,18 @@
  * @returns A grid of the items listed in the gallery database
  */
 
-export default function GalleryGrid(){
+export default async function GalleryGrid() {
     //TO-DO Handle filtering
-    
-    return (<></>)
+      async function fetch(request, env) {
+        const result = await process.env.
+            vgx_feed.prepare(
+                "SELECT * FROM Tags",
+            ).run();
+        return new Response(JSON.stringify(result));
+    }
+    console.log("Testing, testing")
+    console.log("Hye look", fetch({}))
+    return (<>
+
+    </>)
 }
--- src/app/gallery/page.jsx
+++ src/app/gallery/page.jsx
@@ -1,6 +1,8 @@
+
+import GalleryGrid from "./galleryGrid"
 export default function Gallery(){
     return(<div>
         <h3>Gallery</h3>
-        <div></div>
+        <GalleryGrid></GalleryGrid>
     </div>)
 }
