9bed318baf6509ed6ecdd15c2c8266c4938cba8b
Joseph Green <josephg896@gmail.com>
2026-08-21 06:50:39 -0500

    Adjusting the configuration to allow for Server Actions

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,11 @@
 import type { NextConfig } from "next";
 
+module.exports = {
+  experimental: {
+    serverActions: true,
+  },
+}
+
 const nextConfig: NextConfig = {
 	/* config options here */
 };
--- src/app/upload/tagManager.jsx
+++ src/app/upload/tagManager.jsx
@@ -5,16 +5,14 @@
 import AddTag from "./addTagButton"
 import {getTags} from "./actions"
 
-export default async function TagManager({tags}){
-    //var tags = await getCloudflareContext(true).env.vgx_feed.prepare("SELECT * FROM Tags").run();
+export default async function TagManager(){
     console.log("tags be ", tags)
-    //return (<></>);
-    
+
     return (<div className="groupDataInput">
         <select multiple>
-            {tags.map((el)=>(
+            {getTags().then((em) => em.map((el)=>(
                 <option key={el.tagName}>{el.tagDisplay}</option>
-            ))}
+            )))}
 
         </select>
         <AddTag></AddTag>
