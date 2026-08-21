0f515996f213bcab483653d213fae053320ad93a
Joseph Green <josephg896@gmail.com>
2026-08-21 13:10:25 -0500

    Process env will work?

--- src/app/upload/tagManager.jsx
+++ src/app/upload/tagManager.jsx
@@ -7,9 +7,8 @@ import { getCloudflareContext } from "@opennextjs/cloudflare";
 
 export default async function TagManager(){
     async function getTags(){
-        var ob = {async: true}
-        var res = await getCloudflareContext(ob).env?.vgx_feed?.prepare("SELECT * FROM Tags").run();
-        return res?.results || [];
+        var res = await process.env.vgx_feed.prepare("SELECT * FROM Tags").run();
+        return res.results;
     }
     return (<div className="groupDataInput">
         <select multiple>
