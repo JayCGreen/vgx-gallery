c9f024a3c08597ef82e4bedd4c3e5729655f464c
Joseph Green <josephg896@gmail.com>
2026-08-21 06:32:34 -0500

    Switch from react file to regular javascript to see if that helps

---
+++ src/app/upload/actions.js
@@ -0,0 +1,29 @@
+
+'use server'
+
+import {getCloudflareContext} from "@opennextjs/cloudflare";
+
+
+export async function getCollections(){
+    var res = await getCloudflareContext(true).env.vgx_feed.prepare("SELECT * FROM Collections").run()
+    return res.results;
+}
+
+export async function getTags(){
+    var res = await getCloudflareContext(true).env.vgx_feed.prepare("SELECT * FROM Tags").run();
+    return res.results;
+}
+
+
+
+export async function addTag(formData){
+    var res = await getCloudflareContext().env.vgx_feed.prepare("INSERT INTO Tags (tagName, tagDisplay) VALUES (?, ?)").bind(formData.get("tagId"), formData.get("tagName")).run();
+    //var res = await getCloudflareContext().env.vgx_feed.prepare("Select * from Tags").run();
+    console.log("Data Ran" + JSON.stringify(res.results))
+}
+
+export async function addCollection(formData){
+    var res = await getCloudflareContext().env.vgx_feed.prepare("INSERT INTO Collections (collectionName, collectionDisplay) VALUES (?, ?)").bind(formData.get("collectionId"), formData.get("collectionName")).run();
+    //var res = await getCloudflareContext().env.vgx_feed.prepare("Select * from Tags").run();
+    console.log("Data Ran" + JSON.stringify(res.results))
+}
