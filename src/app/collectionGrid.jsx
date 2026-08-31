9532cb7a59df9ec2d030ffdd5d5f9160c0e3c816
Joseph Green <josephg896@gmail.com>
2026-08-30 19:43:35 -0500

    Good enough css fix with the collections, also locking the menus

--- src/app/collectionGrid.jsx
+++ src/app/collectionGrid.jsx
@@ -1,14 +1,17 @@
 
-import { getCloudflareContext } from "@opennextjs/cloudflare";
 import style from "./collectionGrid.module.css"
 
 export default async function CollectionGrid({ items }) {
+
     return (
         <>
             <ul className={style.collectionsList}>
                 {items.map((el) => (
                     <li key={`collection${el.collectionId}`} className={style.collectionItemContainer} >
                         <a href={`posts?c=${el.collectionId}`} className={style.collectionItem}>
+                            {el.source ? <div className={style.collectionCover}>
+                            <img className={style.collectionImage} src={el.source}></img>
+                        </div> : null}
                             <h4 style={{ textAlign: "center" }}>{el.collectionDisplay}</h4>
                         </a>
                     </li>
--- src/app/featureCollections.jsx
+++ src/app/featureCollections.jsx
@@ -4,15 +4,34 @@ import CollectionGrid from "./collectionGrid"
 
 export default async function FeaturedCollections() {
     //Get 3 Collections, will sort out how we get them another time
+    //Would Likely want to replace this with like a join down the line
     const { env } = await getCloudflareContext({ async: true });
     const collections = (await env.vgx_feed.prepare(
-        "SELECT * FROM Collections LIMIT 3"
+        "SELECT * FROM Collections LEFT JOIN Banners ON Collections.collectionId = Banners.collection LIMIT 3 "
     ).run()).results;
-    console.log("colllection be",  collections)
+    console.log("colllection be", collections);
+
+    var itemList = await Promise.all(collections.map(async (el) => {
+        if(el.media = null){
+            return {... el}
+        }
+        var imgSource
+        const mediaList = (await env.vgx_feed.prepare(
+            "SELECT * FROM Media WHERE mediaId = ?"
+        ).bind(el.media).run()).results;
+        if (mediaList.length > 0) {
+            var mediaUrl = await env.vgx_r2?.get(mediaList[0].r2Id);
+            var contentType = mediaUrl?.httpMetadata.contentType;
+            var uri = await mediaUrl.arrayBuffer();
+            imgSource = `data:${contentType};base64, ${Buffer.from(uri).toString('base64')}`;
+        }
+        return {...el, source: imgSource}
+    }))
+
     return (
-    <>
-    <h3>Collections</h3>
-        <CollectionGrid items={collections}></CollectionGrid>
-    
-    </>)
+        <>
+            <h3>Collections</h3>
+            <CollectionGrid items={itemList}></CollectionGrid>
+
+        </>);
 }
