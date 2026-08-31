7a01105315249de3702ca621a9a073152cdfcfab
Joseph Green <josephg896@gmail.com>
2026-08-31 15:34:10 -0500

    Added infinite scroll to the Posts page

--- src/app/postGrid.jsx
+++ src/app/postGrid.jsx
@@ -1,32 +1,68 @@
-import styles from "./postGrid.module.css"
+'use client';
+import styles from "./postGrid.module.css";
+import { useState, useEffect } from 'react';
 
-export default function PostGrid({ items }) {
+export default function PostGrid({ items, pageSize, total, showMore }) {
+    const [itemList, setItemList] = useState(items)
+    const [offset, setOffset] = useState(pageSize)
+    console.log(offset, total, offset < total)
+    useEffect(() => {
+        if (showMore) {
+            window.addEventListener('scroll', scrolls)
+            return () => {
+                window.removeEventListener('scroll', scrolls);
+            }
+        }
+    }, [itemList, offset])
     return (
-        <ul className={styles.postGrid}>
-            {items.map((post) =>
-            (
-                <li key={`post${post.postId}`} className={styles.gridPostContainer}>
-                    <a className={styles.gridPost} href={`/p/${post.postId}`}>
+        <div className={styles.component}>
+            <ul className={styles.postGrid}>
+                {itemList.map((post) =>
+                (
+                    <li key={`post${post.postId}`} className={styles.gridPostContainer}>
+                        <a className={styles.gridPost} href={`/p/${post.postId}`}>
 
-                        {post.source ?
-                            <div className={styles.postMediaContainer}>
-                                <img className={styles.postImage} src={post.source}></img>
-                            </div> : null}
-                        <div className={styles.gridPostContent}>
-                            <div className={styles.gridPostHeader}>
-                                <h4>{post.title}</h4> <p>{post.postDate.split(" ")[0]}</p>
+                            {post.source ?
+                                <div className={styles.postMediaContainer}>
+                                    <img className={styles.postImage} src={post.source}></img>
+                                </div> : null}
+                            <div className={styles.gridPostContent}>
+                                <div className={styles.gridPostHeader}>
+                                    <h4>{post.title}</h4> <p>{post?.postDate?.split(" ")[0]}</p>
+                                </div>
+                                <span className={styles.gridPostDesc}>
+                                    {post.desc}
+                                </span>
+
                             </div>
-                            <span className={styles.gridPostDesc}>
-                                {post.desc}
-                            </span>
 
-                        </div>
-
-                    </a>
-                </li>
-            )
-            )}
-        </ul>
+                        </a>
+                    </li>
+                )
+                )}
+            </ul>
+            <div className={styles.pageControl}>
+                {(showMore) ? <span>{offset} of {total}</span> : null}
+                {(showMore && ((offset) < total)) ? <button onClick={loadMore}>Load More</button> : null}
+            </div>
+        </div>
 
     )
+    async function loadMore() {
+        if (offset < total) {
+            var newPosts = await fetch(`actions/posts?offset=${offset}&limit=${pageSize}`);
+            var a = await newPosts.json()
+            console.log("the adder is", a)
+            setItemList(() => itemList.concat(a.posts));
+            setOffset(() => offset + a.posts.length)
+        }
+    }
+
+    async function scrolls(ev) {
+        console.log("triggered at least")
+        if ((window.innerHeight + window.pageYOffset) >= document.body.offsetHeight) {
+            await loadMore();
+        }
+    };
+
 }
--- src/app/posts/page.jsx
+++ src/app/posts/page.jsx
@@ -5,33 +5,31 @@ import HeroBanner from "../heroBanner";
 
 export default async function PostCat({ searchParams }) {
     const filters = (await searchParams);
+    const pageSize = 4;
     console.log("filters be ", filters)
     const { env } = await getCloudflareContext({ async: true });
     const postList = (await env.vgx_feed.prepare(
         filterPosts(filters)
     ).run()).results;
-    const mediaList = (await env.vgx_feed.prepare(
-        "SELECT * FROM Media"
-    ).run()).results;
+    console.log("items be", postList)
+    const count = await getTotal(filters)
 
     const collectionDisplay = filters.c ? (await env.vgx_feed.prepare(
         "SELECT collectionDisplay FROM Collections WHERE collectionId=?"
     ).bind(filters.c).run()).results : [];
     //should probably extract this little method too, or have it hooked up to the sql statement
     var itemList = await Promise.all(postList.map(async (post) => {
-        var media = mediaList.filter((el) => {
-            return el.post == post.postId
-        })
-        //Get the media Url
+        //will need to handle unique post at a later day
         var imgSource;
-        if (media.length > 0) {
-            var mediaUrl = await env.vgx_r2?.get(media[0].r2Id);
+        if (post.r2Id) {
+            var mediaUrl = await env.vgx_r2?.get(post.r2Id);
             var contentType = mediaUrl.httpMetadata.contentType;
             var uri = await mediaUrl.arrayBuffer();
             imgSource = `data:${contentType};base64, ${Buffer.from(uri).toString('base64')}`;
         }
         return { ...post, source: imgSource }
     }))
+
 
     return (
         <div className="postPage">
@@ -40,17 +38,33 @@ export default async function PostCat({ searchParams }) {
             <h3>
                 {collectionDisplay.length > 0 ? collectionDisplay[0].collectionDisplay : null} Posts
             </h3>
-            <PostGrid items={itemList}></PostGrid>
+            <PostGrid items={itemList} pageSize={pageSize} showMore={true} total={count}></PostGrid>
         </div>
     )
 
-    function filterPosts(filter) {
-        var query = "SELECT * FROM Post ";
+    async function getTotal(filter) {
+        var query = "SELECT count(*) as total FROM Post ";
         if (filter?.c) {
             query += `JOIN CollectionPosts ON CollectionPosts.post = Post.postId WHERE CollectionPosts.collection=${filter.c} `;
         }
         //something for the tags
         query += `ORDER BY julianday(Post.postDate) DESC`
+
+        const count = (await env.vgx_feed.prepare(
+            query
+        ).run()).results;
+        console.log("count be ",count)
+        return count[0].total;
+    }
+
+    function filterPosts(filter) {
+        var query = "SELECT * FROM Post LEFT JOIN Media on Post.postId = Media.post ";
+        if (filter?.c) {
+            query += `JOIN CollectionPosts ON CollectionPosts.post = Post.postId WHERE CollectionPosts.collection=${filter.c} `;
+        }
+        query
+        //something for the tags
+        query += `ORDER BY julianday(Post.postDate) DESC LIMIT ${pageSize}`
         return query;
     }
 }
