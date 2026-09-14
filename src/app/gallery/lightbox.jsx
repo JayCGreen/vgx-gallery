af80faf4f41ef538713e2b508d68008176b666fa
Joseph Green <josephg896@gmail.com>
2026-09-14 13:59:43 -0500

    Snap scroller for mobile viewers, part 1

--- src/app/gallery/lightbox.jsx
+++ src/app/gallery/lightbox.jsx
@@ -6,12 +6,13 @@ import PillBox from "../pillbox"
 export default function Lightbox({ items, index, setIndex }) {
     //const [lightboxIndex, setIndex] = useState(index);
     const [showInfo, setShowInfo] = useState(false)
-    const post = items[index]
+    //const post = items[index]
     /*
     useEffect(()=>{
         setIndex(index)
     }, [index])
     */
+
     const len = items?.length;
     console.log(items[index])
     console.log("collecion is", items[index]?.collections)
@@ -21,37 +22,42 @@ export default function Lightbox({ items, index, setIndex }) {
         }
         console.log("hey am I seen")
         window.addEventListener("scrollend", handSwipe)
+        console.log("testing", document.getElementsByClassName("activeImg")[0])
+        document.getElementsByClassName("activeImg")[0]?.scrollIntoView();
         return () => window.removeEventListener("scrollend", handSwipe)
-    }, [])
+    }, [index])
 
     return (<>
         {index != undefined ? <div className={style.lightbox}>
             <div className={style.topControls}>
                 <button onClick={() => setShowInfo(!showInfo)}> Info</button>
                 <div>
-                    <button className={style.lightboxControls} onClick={() => setIndex((((index - 1) % len) + len) % len)}>&larr;</button>
-                    <button className={style.lightboxControls} onClick={() => setIndex((((index + 1) % len) + len) % len)}> &rarr;</button>
+                    <button className={style.lightboxControls} onClick={() => setIndex((((index - 1) % len) + len) % len)}>&#8896;</button>
+                    <button className={style.lightboxControls} onClick={() => setIndex((((index + 1) % len) + len) % len)}> &#8897;</button>
                 </div>
                 <button onClick={() => setIndex()}>Exit</button>
 
             </div>
-            <div className={style.lightboxContent}>
-                <img className={style.lightboxImg} src={items[index].uri}></img>
-                {showInfo ? <div className={style.postInfo}>
-                    <div className={style.postHeader}>
-                        <h2>{post.title}</h2>
-                        <p>{post.uploadDate?.split(" ")[0]}</p>
-                    </div>
-                    <p>{post.description}</p>
-                    {post.collections?.length > 0 ? <div>
-                        <h5>Collections</h5>
-                        <PillBox editable={false} group={post.collections}></PillBox>
-                    </div> : null}
-                    {post.tags?.length > 0 ? <div>
-                        <h5>Tags</h5>
-                        <PillBox editable={false} group={post.tags}></PillBox>
-                    </div> : null}
-                </div> : null}
+            <div className={style.lightScroller}>
+                {items.map((post, i) => (
+                    <div key={`lb_${post.postId}`} className={`${style.lightboxContent} ${i == index ? "activeImg" : ""}`}>
+                        <img className={style.lightboxImg} src={post.uri}></img>
+                        {showInfo ? <div className={style.postInfo}>
+                            <div className={style.postHeader}>
+                                <h2>{post.title}</h2>
+                                <p>{post.uploadDate?.split(" ")[0]}</p>
+                            </div>
+                            <p>{post.description}</p>
+                            {post.collections?.length > 0 ? <div>
+                                <h5>Collections</h5>
+                                <PillBox editable={false} group={post.collections}></PillBox>
+                            </div> : null}
+                            {post.tags?.length > 0 ? <div>
+                                <h5>Tags</h5>
+                                <PillBox editable={false} group={post.tags}></PillBox>
+                            </div> : null}
+                        </div> : null}
+                    </div>))}
             </div>
         </div> : null}
     </>)
