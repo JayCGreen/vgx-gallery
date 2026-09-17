685e9b41c3b711b5f5ee8c66c321b8ca05d140dd
Joseph Green <josephg896@gmail.com>
2026-09-17 15:00:12 -0500

    Upload page adjustment part 1

--- src/app/pillbox.jsx
+++ src/app/pillbox.jsx
@@ -2,11 +2,13 @@ import style from "./pillbox.module.css"
 
 export default function PillBox({ editable, group, setGroup }) {
     return (<div className={style.pillbox}>
-        <h4 style={{margin: "unset", alignContent: "center"}}>{group[0].tagName ? "Tags" : "Collections"}: </h4>
+        {group[0] && (group[0].tagName || group[0].collectionName) ?
+            <h4 style={{ margin: "unset", alignContent: "center" }}>{group[0]?.tagName ? "Tags" : "Collections"}: </h4> : null
+        }
         {
-            
+
             group.map((el) => (
-                <div key={el.tagName || el.collectionName} className={style.pill }>
+                <div key={el.tagName || el.collectionName} className={style.pill}>
                     {editable ? <button onClick={(e) => {
 
                         setGroup(group.filter((a) => el != a))
